4448e9d5e0e725613fc58d2735a76ce76dcd409a
Artur Rodrigues Rocha Neto <artur.rodrigues26@gmail.com>
2018-09-06 15:49:26 -0300

    codigos em R do matheus

--- homework1/homework1.py
+++ homework1/homework1.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
 
 # paths e arquivos
 figpath = "figures/"
@@ -13,13 +16,24 @@ df = df.drop(["id"], axis=1)
 # lista de preditores (exclui-se a coluna da classe)
 predictors = list(df.drop(["class"], axis=1).columns.values)
 
+# PCA
+X = np.array(df.drop(["class"], axis=1))
+X = scale(X, axis=1)
+pca = PCA()
+pca.fit(X)
+print(pca.explained_variance_ratio_)
+print(np.sum(pca.explained_variance_ratio_))
+plt.bar(range(1,10), pca.explained_variance_ratio_)
+plt.show()
+exit()
+
 # analise monovariada
 # - histograma
 # - media
 # - desvio padrao
 # - assimetria
 columns = ["predictor", "mean", "std", "skewness"]
-analyzers = pd.DataFrame(columns=columns)
+monovariate = pd.DataFrame(columns=columns)
 for p in predictors:
 	fig = plt.figure()
 	plt.hist(df[p], bins=5)
@@ -33,8 +47,8 @@ for p in predictors:
 	skewness = df[p].skew()
 
 	new_entry = pd.DataFrame([[p, mean, std, skewness]], columns=columns)
-	analyzers = analyzers.append(new_entry)
+	monovariate = analyzers.append(new_entry)
 
 
 # debuf
-analyzers.to_csv(resfile, index=False)
+monovariate.to_csv(resfile, index=False)
